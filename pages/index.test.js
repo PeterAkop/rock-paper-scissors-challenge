7117a5abf440bb01a0633e86ad31f23006f2b369
peterakop <peterakoup@gmail.com>
2021-01-19 21:13:54 +0000

    remove unused import

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,9 +1,9 @@
-import { getByTestId, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Home from "../pages/index";
 
 describe("App", () => {
   it("home page is rendered", () => {
-    const {queryByTestId} = render(<Home />);
+    const { queryByTestId } = render(<Home />);
     expect(queryByTestId(/home/i)).toBeTruthy();
   });
 });
