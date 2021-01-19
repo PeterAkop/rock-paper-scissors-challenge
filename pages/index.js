20046494f4b9d077101ea2efbc23e2db5441db17
peterakop <peterakoup@gmail.com>
2021-01-19 21:12:38 +0000

    setup tests

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Rock Paper Scissors</h1>
+        <h1 data-testid={'home-id'}>Rock Paper Scissors</h1>
         <Link href="/game" to="/game">Play!</Link>
       </main>
     </div>
---
+++ pages/index.test.js
@@ -0,0 +1,9 @@
+import { getByTestId, render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("App", () => {
+  it("home page is rendered", () => {
+    const {queryByTestId} = render(<Home />);
+    expect(queryByTestId(/home/i)).toBeTruthy();
+  });
+});
